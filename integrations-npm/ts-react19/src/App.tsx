cfdb54d6754ddc2bdca16404b76eb97b13573a5b
Jurij Koch <jurij@uq.dev>
2025-03-25 20:44:50 +0100

    add scatter chart from storybook

--- integrations-npm/ts-react19/src/App.tsx
+++ integrations-npm/ts-react19/src/App.tsx
@@ -1,7 +1,28 @@
 import './App.css';
-import { Bar, BarChart, Legend, Tooltip, XAxis, YAxis } from 'recharts';
+import {
+  Bar,
+  BarChart,
+  Legend,
+  CartesianGrid,
+  ResponsiveContainer,
+  Scatter,
+  ScatterChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
 
 function App() {
+  const data = [
+    { x: 100, y: 200, z: 200 },
+    { x: 100, y: 200, z: 200 },
+    { x: 120, y: 100, z: 260 },
+    { x: 170, y: 300, z: 400 },
+    { x: 140, y: 250, z: 280 },
+    { x: 150, y: 400, z: 500 },
+    { x: 110, y: 280, z: 200 },
+  ];
+
   return (
     <>
       <h1>Vite + React 19 + TypeScript + Recharts</h1>
@@ -19,6 +40,23 @@ function App() {
         <Legend />
         <Tooltip />
       </BarChart>
+      <ResponsiveContainer width="100%" height={400}>
+        <ScatterChart
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <CartesianGrid />
+          <XAxis type="number" dataKey="x" name="stature" unit="cm" />
+          <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} defaultIndex={1} />
+          <Scatter name="A school" data={data} fill="#8884d8" />
+          <Legend />
+        </ScatterChart>
+      </ResponsiveContainer>
     </>
   );
 }
